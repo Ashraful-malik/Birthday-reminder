487d3edab472c19ebda90edfbd65d59748c6655b
Ashraful-malik <ashrafulmalik71@gmail.com>
2020-11-25 10:55:59 +0530

    change on main.py file and birthday.db

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tkinter import *
+from tkinter import Label, Entry, Button, Tk, Frame, TOP, END
 import sqlite3
 from tkinter import messagebox
 window = Tk()
@@ -7,7 +7,7 @@ window = Tk()
 def add_friend():
     connect = sqlite3.connect('birthday.db')
     cursor = connect.cursor()
-    add=cursor.execute("INSERT INTO birthday VALUES(:name_box,:birthday_box)",
+    cursor.execute("INSERT INTO birthday VALUES(:name_box,:birthday_box)",
                    {
                        'name_box': name_box.get(),
                        'birthday_box': birthday_box.get()
@@ -44,24 +44,23 @@ window.geometry("590x300")
 connect = sqlite3.connect('birthday.db')
 cursor = connect.cursor()
 
-# cursor.execute("""CREATE TABLE birthday(
-#     Friend_name text,
-#     Birthday text
-#         )""")
 
-
-frame = Frame(window,bg='#33ffbd')
+frame = Frame(window, bg='#33ffbd')
 frame.pack(side=TOP)
-name_label = Label(frame, text="Name of your Friend", font=" arial 20 italic",bg='#33ffbd',fg="blue")
-Birthday_label = Label(frame, text="Birthday", font=" arial 20 italic",bg='#33ffbd',fg="blue")
+name_label = Label(frame, text="Name of your Friend",
+                   font=" arial 20 italic", bg='#33ffbd', fg="blue")
+Birthday_label = Label(frame, text="Birthday",
+                       font=" arial 20 italic", bg='#33ffbd', fg="blue")
 
 name_box = Entry(frame, width=30, font="20")
 birthday_box = Entry(frame, width=30, font='20')
 Add_btn = Button(frame, text="Add Friend Birthday", width=30, command=add_friend,
-                 bg='#337dff',font="arial 15",fg="white")
-search_friend_box = Entry(frame, text="Add Friend Birthday", width=30,font=' Courier 13')
+                 bg='#337dff', font="arial 15", fg="white")
+search_friend_box = Entry(
+    frame, text="Add Friend Birthday", width=30, font=' Courier 13')
 
-search_btn = Button(frame, text="SEARCH NAME", command=search_name,font='Roboto 10',bg='#34d2eb',relief='flat')
+search_btn = Button(frame, text="SEARCH NAME", command=search_name,
+                    font='Robot 10', bg='#34d2eb', relief='flat')
 
 name_label.grid(row=0, column=0)
 Birthday_label.grid(row=1, column=0)
@@ -69,7 +68,7 @@ name_box.grid(row=0, column=1, pady=20)
 birthday_box.grid(row=1, column=1, pady=20)
 Add_btn.grid(row=2, column=0, columnspan=3)
 
-search_btn.grid(row=3, column=0, columnspan=1,pady=10)
+search_btn.grid(row=3, column=0, columnspan=1, pady=10)
 search_friend_box.grid(row=3, column=1)
 window.config(bg='#33ffbd')
 connect.commit()
